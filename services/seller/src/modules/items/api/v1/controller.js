0d7b5e073d5bbaf9d800c8b8a57c71eda7d46805
OmeshSachethana <sachethana12@gmail.com>
2024-04-21 22:43:19 +0530

    added getItems controller

--- services/seller/src/modules/items/api/v1/controller.js
+++ services/seller/src/modules/items/api/v1/controller.js
@@ -6,6 +6,8 @@ import { objectIdSchema } from "@app/constants";
 import { toSuccess } from "@app/middleware";
 import {
   serviceCreateItem,
+  serviceGetItems,
+  serviceGetItemById,
 } from "./service";
 
 import { createItemSchema, updateItemSchema } from "./schema";
@@ -20,6 +22,30 @@ item.post(
     return toSuccess({ res, data, message: "Item created successfully!" });
   })
 );
+
+item.get(
+  "/",
+  filterQuery,
+  asyncHandler(async function controllerGetReviews(req, res) {
+    const data = await serviceGetItems(
+      req.query.filter,
+      req.query.sort,
+      req.query.page,
+      req.query.limit
+    );
+    return toSuccess({ res, data, message: "Items fetched successfully!" });
+  })
+);
+
+item.get(
+  "/:id",
+  celebrate({ [Segments.PARAMS]: objectIdSchema() }),
+  asyncHandler(async function controllerGetReviewById(req, res) {
+    const data = await serviceGetItemById(req.params.id);
+    return toSuccess({ res, data, message: "Item fetched successfully!" });
+  })
+);
+
 
 
 
