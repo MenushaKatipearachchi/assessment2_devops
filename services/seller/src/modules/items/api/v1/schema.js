afea625619350045453dcf8428cc613b397137a3
OmeshSachethana <sachethana12@gmail.com>
2024-04-21 19:56:20 +0530

    created ItemSchema

---
+++ services/seller/src/modules/items/api/v1/schema.js
@@ -0,0 +1,12 @@
+import { Joi } from "celebrate";
+
+export const createItemSchema = Joi.object({
+  title: Joi.string().optional(),
+  message: Joi.string().optional(),
+  category: Joi.string().optional(),
+  price: Joi.number().required(),
+  seller: Joi.string().required(),
+  selectedFile: Joi.string().required(),
+  avgRating: Joi.number().optional().default(0),
+});
+
