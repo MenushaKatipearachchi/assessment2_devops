a396de0216ac7fe8d259ad70d23148afcaaef978
OmeshSachethana <sachethana12@gmail.com>
2024-04-21 22:41:13 +0530

    added getItems service

--- services/seller/src/modules/items/api/v1/service.js
+++ services/seller/src/modules/items/api/v1/service.js
@@ -3,3 +3,11 @@ import { createItem } from "../../repository";
 export const serviceCreateItem = (review) => {
   return createItem(review);
 };
+
+export const serviceGetItems = (filters, sorts, page, limit) => {
+  return getAllItems({ filters, sorts, page, limit });
+};
+
+export const serviceGetItemById = (id) => {
+  return getItemById(id);
+};
