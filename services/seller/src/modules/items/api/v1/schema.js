b1fed34f5c1527c941fd5df32b50fc2a84716b43
OmeshSachethana <sachethana12@gmail.com>
2024-04-21 19:58:13 +0530

    created updateItemSchema

--- services/seller/src/modules/items/api/v1/schema.js
+++ services/seller/src/modules/items/api/v1/schema.js
@@ -10,3 +10,12 @@ export const createItemSchema = Joi.object({
   avgRating: Joi.number().optional().default(0),
 });
 
+export const updateItemSchema = Joi.object({
+  title: Joi.string().optional(),
+  message: Joi.string().optional(),
+  category: Joi.string().optional(),
+  price: Joi.number().optional(),
+  seller: Joi.string().optional(),
+  selectedFile: Joi.string().optional(),
+  avgRating: Joi.number().optional(),
+});
